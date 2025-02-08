208118454ac2a3b9f626aad627e1633b6a94a982
Kevin Taylor <ktaylor323@gatech.edu>
2025-02-07 21:11:14 -0600

    [KT] Initial Repo Setup and bootstrap

---
+++ backend/globalyouthleadership/src/test/java/com/legacyinternational/globalyouthleadership/GlobalyouthleadershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legacyinternational.globalyouthleadership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalyouthleadershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
