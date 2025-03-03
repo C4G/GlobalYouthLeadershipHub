2c1b11e51452b221988082796a56fe8874a1bf93
Kevin Taylor <ktaylor323@gatech.edu>
2025-03-02 19:59:47 -0600

    [kt] Automated flyway and dev profile enabled

--- backend/globalyouthleadership/src/main/java/com/legacyinternational/globalyouthleadership/GlobalyouthleadershipApplication.java
+++ backend/globalyouthleadership/src/main/java/com/legacyinternational/globalyouthleadership/GlobalyouthleadershipApplication.java
@@ -2,8 +2,10 @@ package com.legacyinternational.globalyouthleadership;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.legacyinternational.globalyouthleadership.infrastructure.repositories")
 public class GlobalyouthleadershipApplication {
 
 	public static void main(String[] args) {
