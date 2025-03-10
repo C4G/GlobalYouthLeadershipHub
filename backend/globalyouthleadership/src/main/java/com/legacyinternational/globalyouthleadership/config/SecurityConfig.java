15fbd59847760ce1a96e178c79ad6ef6ac0a35ce
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2025-03-10 03:47:10 +0000

    Compile frontend assets into backend Dockerfile

--- backend/globalyouthleadership/src/main/java/com/legacyinternational/globalyouthleadership/config/SecurityConfig.java
+++ backend/globalyouthleadership/src/main/java/com/legacyinternational/globalyouthleadership/config/SecurityConfig.java
@@ -4,30 +4,28 @@ import com.legacyinternational.globalyouthleadership.service.authentication.JwtF
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.util.List;
-
 @Configuration
 public class SecurityConfig {
+
+    private static final String[] FRONTEND_ASSETS = { "/", "/index.html", "/assets/**", "/organizationLogo.jpg" };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
         http
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/api/ping").permitAll() // Allow without authentication
+                        .requestMatchers(FRONTEND_ASSETS).permitAll()
+                        // .requestMatchers("/api/ping").permitAll() // Allow without authentication
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/api/auth/login", "/api/auth/register").permitAll()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN") // Admin access
