df73841be9997e8e39be07c3ffdc4b30f7f22066
Jun Siang Neo <jneo6@gatech.edu>
2025-03-27 21:29:07 +0800

    Init Project Page Skeleton

---
+++ frontend/src/pages/ProjectPage.jsx
@@ -0,0 +1,27 @@
+import Sidebar from "@/components/Sidebar";
+// import Spinner from "@/components/Spinner";
+// import { useGetProjectByUserId } from "@/hooks/projects";
+import styles from "@/styles/pages/ProjectPage.module.css"
+
+const ProjectPage = () => {
+
+    {/* TODO - to enable once API is wire up */ }
+    // const { data, isLoading } = useGetProjectByUserId()
+
+    {/* TODO - to enable once API is wire up */ }
+    // if (isLoading) {
+    //     return <Spinner text={"Fetching User Project..."} />
+    // }
+
+    return (
+        <div className={styles.container}>
+            <Sidebar />
+            <main className={styles.mainContent}>
+                {/* TODO - to enable once API is wire up */}
+            </main>
+        </div>
+    );
+
+}
+
+export default ProjectPage
