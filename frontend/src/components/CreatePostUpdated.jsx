b7980336d1ce76c21ea8297475b124741bb277f6
Jun Siang Neo <jneo6@gatech.edu>
2025-03-27 21:29:35 +0800

    Init Create Post Components

---
+++ frontend/src/components/CreatePostUpdated.jsx
@@ -0,0 +1,47 @@
+import styles from "@/styles/components/CreatePostUpdated.module.css"
+import { useMutation } from "@tanstack/react-query"
+import { useState } from "react"
+
+const CreatePostUpdated = () => {
+    const [openForm, setOpenForm] = useState(false)
+
+    const mutation = useMutation({
+        mutationFn: async () => { },
+        onSuccess: () => setOpenForm(prev => !prev),
+        onError: () => { }
+    })
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        mutation.mutate()
+    }
+
+    return (
+        <>
+            {!openForm &&
+                <div className={styles.postCard}>
+                    <button
+                        className={styles.createPostButton}
+                        onClick={() => setOpenForm(!openForm)}>
+                        Create New Post
+                    </button>
+                </div>
+            }
+
+            {openForm &&
+                <div className={styles.formContainer}>
+                    <form className={styles.form}>
+                        <h3>Create a Post</h3>
+                        <input type="text" placeholder="Post Name" required />
+                        <textarea placeholder="Post Description"></textarea>
+                        <button type="submit" onSubmit={handleSubmit}>Submit</button>
+                        <button type="button" onClick={() => setOpenForm(prev => !prev)}>Cancel</button>
+                    </form>
+                </div>
+            }
+        </>
+    )
+}
+
+export default CreatePostUpdated
