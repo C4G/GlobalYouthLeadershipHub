518291ffe541a4500967fca2fb522b8cf682b041
Jun Siang Neo <jneo6@gatech.edu>
2025-03-30 00:21:47 +0800

    Init ProjectList Component

---
+++ frontend/src/components/ProjectList.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import Spinner from "@/components/Spinner";
+import EmptyList from "@/components/EmptyList";
+import ProjectCard from "@/components/ProjectCard";
+
+const ProjectList = ({ projects, isLoading }) => {
+    if (isLoading) {
+        return (
+            <>
+                <Spinner text={"Fetching all Projects..."} />
+            </>
+        )
+    }
+
+    return (
+        <>
+            {projects.length === 0 && <EmptyList text={"No Projects Found"} />}
+            {projects.map(project => {
+                return (
+                    <>
+                        <ProjectCard key={project.id} project={project} />
+                    </>
+                )
+            })}
+        </>
+    )
+}
+
+export default ProjectList
