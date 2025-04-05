c1ecb0eca9dfcda8be1c09369b0a07c44b9ecd61
Jun Siang Neo <jneo6@gatech.edu>
2025-04-05 22:03:59 +0800

    Added Fallback Component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,19 @@
 import { createRoot } from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
 import { StrictMode } from 'react';
-import App from "@/App.jsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "@/libs/queryClient.js";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ENV } from "@/constants/env.js";
+import App from "@/App.jsx";
+import Fallback from "@/components/Fallback";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <App />
+            <ErrorBoundary fallback={<Fallback />}>
+                <App />
+            </ErrorBoundary>
             {ENV === 'local' && <ReactQueryDevtools initialIsOpen={true} />}
         </QueryClientProvider>
     </StrictMode>
