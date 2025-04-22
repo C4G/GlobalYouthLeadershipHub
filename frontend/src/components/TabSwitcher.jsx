52188274e7b154e46336dd8e43c8058962b05cf3
Jun Siang Neo <jneo6@gatech.edu>
2025-04-23 00:25:09 +0800

    Init Reusable TabSwitcher

---
+++ frontend/src/components/TabSwitcher.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import styles from "@/styles/components/TabSwitcher.module.css"
+
+const TabSwitcher = ({ tabs, renderContent }) => {
+    const [activeTab, setActiveTab] = useState(tabs[0]);
+
+    return (
+        <div className={styles.tabWrapper}>
+            <div className={styles.tabHeader}>
+                {tabs.map((tab) => (
+                    <button
+                        key={tab}
+                        className={`${styles.tabButton} ${activeTab === tab ? styles.activeTab : ""}`}
+                        onClick={() => setActiveTab(tab)}
+                    >
+                        {tab}
+                    </button>))}
+            </div>
+            <div className={styles.tabContent}>
+                {renderContent(activeTab)}
+            </div>
+        </div>
+    )
+}
+
+export default TabSwitcher
